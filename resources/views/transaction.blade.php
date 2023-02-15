8d4ce538d0da6acec29a09a1fa67a74c79ec7174
t4gz <fulviansyah787@gmail.com>
2023-02-15 14:34:21 +0700

    benerin switch jadi bagus

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -122,21 +122,23 @@
                                 <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                                 <tr>
                                     <td colspan="2">kupon anda </td>
-                                    <td colspan="1"><input type="number" id="jk" class="form-control"
-                                            value="{{ $kupon->quantity_kupon }}" readonly></td>
+                                    <td colspan="1">
+                                        <input type="number" id="jk" class="form-control"
+                                        value="{{ $kupon->quantity_kupon }}" readonly>
+                                    </td>
+                                    <td class="justify-content-left">
                                         @if ($uk == 0)
-                                            <td>
+                                            <td class="form-switch">
                                                {{-- <h1>kosong cuyh</h1> --}}
-                                                <input type="checkbox" data-toggle="switchbutton" checked data-size="lg" id="pakai" value="" disabled onchange="pakaikupon({{$k->diskon}})">pakai
+                                                <input type="checkbox" class="form-check-input" id="pakai" disabled onchange="pakaikupon({{$k->diskon}})">
+                                                 
                                             </td>
                                         @else
-                                            <td>
-                                                <input type="checkbox" id="pakai" onchange="pakaikupon({{$k->diskon}})">pakai
+                                            <td class="form-switch">
+                                                <input type="checkbox" class="form-check-input" id="pakai" onchange="pakaikupon({{$k->diskon}})">
                                             </td>
                                         @endif
-                                    {{-- <td><input type="checkbox" id="pakai"
-                                            onchange="pakaikupon({{ $k->diskon }})">pakai
-                                    </td> --}}
+                                    </td>
                                     <td>
                                         <p id="text" style="display:none">Checkbox is CHECKED!</p>
                                     </td>
