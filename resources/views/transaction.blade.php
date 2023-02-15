144f3753434a618351aacf8b85c8c206c4791d6b
t4gz <fulviansyah787@gmail.com>
2023-02-15 22:05:47 +0700

    SETENGAH JAM WOI

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -21,7 +21,7 @@
                         <div class="row">
                             @foreach ($item as $i)
                                 <div class="card mx-3" style="width: 13rem;">
-                                    <img src="{{ asset('./img/'.$i->img) }}"   class="card-img-top py-3" alt="...">
+                                    <img src="{{ asset('/template/img/'.$i->img) }}"   class="card-img-top py-3" alt="...">
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $i->name }}</h5>
                                         <p class="card-text">{{$i->deskripsi}}
